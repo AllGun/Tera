b0c443f5c5058a63bc72a8415075979d8f662d03
Alfredo Gutemberg <alfredo.gutemberg@gmail.com>
2022-05-28 19:06:45 -0300

    function voltarParaTopo

--- mini-projeto/script.js
+++ mini-projeto/script.js
@@ -1,6 +1,7 @@
 console.log("Hello World!");
 // Conectar informações do botão desenvolvido no arquivo HTML
 let botao = document.getElementById("botao-topo");
+
 // Função nativa do windows chamada onscroll ao scrollar, armazena a função chamada scrollFunction(), que define quando o botão deve aparecer
 window.onscroll = function scrollFunction () {
   // Se, a quantidade de pixels percorridos na tela for igual à condição passada entre parentêses, então:
@@ -16,5 +17,6 @@ window.onscroll = function scrollFunction () {
 
 // Quando a pessoa clicar em voltar para o topo, executar esta função
 function voltarParaTopo () {
+  document.documentElement.scrollTop = 0; 
 }
 
