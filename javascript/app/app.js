a38f09424a90bffaf0bb8b4aa393f9409c50bcb6
Alfredo Gutemberg <alfredo.gutemberg@gmail.com>
2022-06-19 15:48:57 -0300

    operadores lógicos

--- javascript/app/app.js
+++ javascript/app/app.js
@@ -70,3 +70,13 @@ console.log(b);
 // true
 b = 10 <= 15;
 console.log(b);
+
+console.log(7 === 7);
+console.log(9 == 9);
+console.log(3 === "3");
+console.log(2 == "2");
+
+console.log(5 !== 5);
+console.log(5 !== "5");
+
+
