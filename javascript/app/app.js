54bd397a9d7ee52966b3282708fdfba0cd229e41
Alfredo Gutemberg <alfredo.gutemberg@gmail.com>
2022-06-19 10:28:15 -0300

    declaração de variáveis

--- javascript/app/app.js
+++ javascript/app/app.js
@@ -1,1 +1,26 @@
-console.log("Hello world na Tera!")
+console.log("Hello world na Tera!");
+
+/* 
+Declarando uma variável
+    var nomeDaVariavel = valor;
+        var: indica o tipo de atribuição.
+        nomeDaVariável: indica o nome daquela variável.
+        = Sinal de Atribuição
+        valor: valor que será atribuído àquela variável.
+*/
+
+var a = 5; //  Aqui estamos declarando uma variável chamada "a" e atribuindo o valor de 5 a ela.
+
+
+var nome = "Alfredo Gutemberg";
+var Nome = "Eva Cristina";
+var idade = 36;
+var Idade = 26;
+
+console.log(`Nome: ${nome}`);
+console.log(`Idade: ${idade}`);
+
+console.log(`Nome: ${Nome}`);
+console.log(`Idade: ${Idade}`);
+
+console.log(Idade + idade);
