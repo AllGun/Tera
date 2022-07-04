f86ce8810b64c1b92e0e39e268f593197ebe25a4
Alfredo Gutemberg <alfredo.gutemberg@gmail.com>
2022-07-04 19:40:54 -0300

    switch -

--- javascript/app/aula03.js
+++ javascript/app/aula03.js
@@ -1,1 +1,11 @@
 console.log("Hello f* World!");
+
+
+let operador = "-";
+let number1 = 5, number2 = 6;
+
+switch(operador) {
+    case "-"
+    console.log(`Resultado de  ${number1} - ${number2} = ${number1 - number2}`);
+    break
+}
