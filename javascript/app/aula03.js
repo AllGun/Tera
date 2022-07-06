00ea98d2d9ecd51e3ede389e82f27c9f094840ff
Alfredo Gutemberg <alfredo.gutemberg@gmail.com>
2022-07-06 20:58:16 -0300

    for with list

--- javascript/app/aula03.js
+++ javascript/app/aula03.js
@@ -1,11 +1,31 @@
 console.log("Hello f* World!");
 
+let operador = "*";
+let number1 = 5,
+  number2 = 6;
 
-let operador = "-";
-let number1 = 5, number2 = 6;
-
-switch(operador) {
-    case "-"
+switch (operador) {
+  case "-":
     console.log(`Resultado de  ${number1} - ${number2} = ${number1 - number2}`);
-    break
+    break;
+  case "+":
+    console.log(`Resultado de ${number1} + ${number2} = ${number1 + number2}`);
+  default:
+    console.log("Operador não encontrado");
 }
+
+let count = 0;
+
+function imparOuPar(number) {
+  
+  if ((number % 2) == 0){
+      console.log("Par");
+  } else {
+      console.log("Ímpar");
+  }
+
+  
+}
+
+imparOuPar(10);
+imparOuPar(11);
---
+++ javascript/app/aula04.js
@@ -0,0 +1,8 @@
+let mList = [1, 2, 3];
+
+for (let index = 0; index < mList.length; index++ ) {
+// mList.push(index);
+console.log(mList);
+}
+
+console.log(mList);
