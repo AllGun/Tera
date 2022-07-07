03b1495b9c07deec7df3a2f9f986d97900af1544
Alfredo Gutemberg <alfredo.gutemberg@gmail.com>
2022-07-07 00:17:43 -0300

    add atributos

--- javascript/app/aula04.js
+++ javascript/app/aula04.js
@@ -6,3 +6,26 @@ console.log(mList);
 }
 
 console.log(mList);
+
+class Produto {
+    constructor(name, price, description, qtd) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+        this.qtd = qtd;
+    }
+}
+
+const produto1 = new Produto();
+console.log(produto1);
+
+
+produto1.name = "Macarrão";
+produto1.value = 3;
+produto1.description = "Macarrão do tipo italiano";
+produto1.qtd = 1;
+
+console.log(produto1);
+
+const produto2 = new Produto("Arroz", 20, "Arroz Tio Jorge", 4);
+console.log(produto2);
